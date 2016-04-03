3dd57b51ae401f8b8d1fda1a7fc7394f5bb1188f
avincenthill <alexanderjvincenthill@gmail.com>
2016-04-02 23:52:19 -0700

    Added files, displays map

---
+++ js/main.js
@@ -0,0 +1,24 @@
+<<<<<<< HEAD
+"use strict";
+
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('mapCanvas'), {
+        center: {
+            lat: 0,
+            lng: 0
+        },
+        zoom: 3
+    });
+=======
+"use strict";
+
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('mapCanvas'), {
+        center: {
+            lat: 0,
+            lng: 0
+        },
+        zoom: 3
+    });
+>>>>>>> 2fe2855a4319ea4be151167db5551bfd6fccfd19
+}
