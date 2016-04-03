f5a89a15027cf22580bc1ff53dfc0d6589c8e1fa
avincenthill <alexanderjvincenthill@gmail.com>
2016-04-03 00:11:57 -0700

    Fixed git merge error

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use strict";
 
 function initMap() {
@@ -9,16 +8,5 @@ function initMap() {
         },
         zoom: 3
     });
-=======
-"use strict";
 
-function initMap() {
-    var map = new google.maps.Map(document.getElementById('mapCanvas'), {
-        center: {
-            lat: 0,
-            lng: 0
-        },
-        zoom: 3
-    });
->>>>>>> 2fe2855a4319ea4be151167db5551bfd6fccfd19
 }
